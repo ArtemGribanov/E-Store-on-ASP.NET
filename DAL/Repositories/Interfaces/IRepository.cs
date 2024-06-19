84e7e01cf937050a007c362523aaa03113367d8d
Kigggysha <kirillbel2004@gmail.com>
2024-06-19 09:39:35 +0300

    Create basic repository

---
+++ DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace DAL.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        IEnumerable<T> Find(Func<T, Boolean> predicate);
+        void Create(T entity);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
