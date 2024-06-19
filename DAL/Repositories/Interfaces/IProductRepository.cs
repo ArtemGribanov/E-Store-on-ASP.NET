2ed249d204bd5032fa07c0ace7aeacc08bbf970f
Ilya Katushenok <anotherfles@gmail.com>
2024-06-19 14:17:08 +0300

    Add product repository interface

---
+++ DAL/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using DAL.Entities;
+
+namespace DAL.Repositories.Interfaces;
+public interface IProductRepository
+{
+	Task<string> GetDescriptionByIdAsync(int id);
+	Task<IEnumerable<Product>> GetProductsByCategoryAsync(Category category);
+}
