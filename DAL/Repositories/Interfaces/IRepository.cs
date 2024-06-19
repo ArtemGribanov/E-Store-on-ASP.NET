1185d5ff80e7496ef9ad32f7fc9fccb0c93dcb02
Kigggysha <kirillbel2004@gmail.com>
2024-06-19 16:30:08 +0300

    Deleted non-async methods

--- DAL/Repositories/Interfaces/IRepository.cs
+++ DAL/Repositories/Interfaces/IRepository.cs
@@ -2,18 +2,12 @@
 {
     public interface IRepository<T>
     {
-        IEnumerable<T> GetAll();
-        T GetById(int id);
-        IEnumerable<T> Find(Func<T, bool> predicate);
-        void Create(T entity);
-        void Update(T item);
-        void Delete(int id);
 
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> FindAsync(Func<T, bool> predicate);
-        Task CreateAsync(T entity);
-        Task UpdateAsync(T item);
-        Task DeleteAsync(int id);
+        void CreateAsync(T entity);
+        void UpdateAsync(T item);
+        void DeleteAsync(int id);
     }
 }
