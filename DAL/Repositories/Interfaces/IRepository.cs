7ec40bce622ab31996035cdd06003c3605d9842b
Kigggysha <kirillbel2004@gmail.com>
2024-06-19 16:23:03 +0300

    Updated basic interface with asynchronous methods

--- DAL/Repositories/Interfaces/IRepository.cs
+++ DAL/Repositories/Interfaces/IRepository.cs
@@ -3,10 +3,17 @@
     public interface IRepository<T>
     {
         IEnumerable<T> GetAll();
-        T Get(int id);
-        IEnumerable<T> Find(Func<T, Boolean> predicate);
+        T GetById(int id);
+        IEnumerable<T> Find(Func<T, bool> predicate);
         void Create(T entity);
         void Update(T item);
         void Delete(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> FindAsync(Func<T, bool> predicate);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T item);
+        Task DeleteAsync(int id);
     }
 }
