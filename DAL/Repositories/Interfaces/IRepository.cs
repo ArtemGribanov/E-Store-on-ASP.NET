5dd85e699e281cc6467e299a9fa090134c647576
Kigggysha <kirillbel2004@gmail.com>
2024-06-20 12:23:45 +0300

    Added SaveChangesAsync

--- DAL/Repositories/Interfaces/IRepository.cs
+++ DAL/Repositories/Interfaces/IRepository.cs
@@ -6,6 +6,7 @@
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> FindAsync(Func<T, bool> predicate);
+        Task<int> SaveChangesAsync(T entity);
         void CreateAsync(T entity);
         void UpdateAsync(T item);
         void DeleteAsync(int id);
