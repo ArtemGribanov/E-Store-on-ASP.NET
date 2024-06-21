cbee88562614d3c834af76bf252617b2a0707697
Ilya Katushenok <anotherfles@gmail.com>
2024-06-21 14:49:13 +0300

    And for IOrderRepository

--- DAL/Repositories/Interfaces/IOrderRepository.cs
+++ DAL/Repositories/Interfaces/IOrderRepository.cs
@@ -4,7 +4,6 @@ namespace DAL.Repositories.Interfaces;
 
 public interface IOrderRepository : IRepository<Order>
 {
-	Task<IEnumerable<Order>> GetOrdersByUserAsync(User user);
-	Task<IEnumerable<Product>> GetProductsByOrderIdAsync(int orderId);
+
 }
 
