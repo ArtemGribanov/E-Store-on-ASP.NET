62766f5c9900f6f56fc586361fb0403038fabbd4
Ilya Katushenok <anotherfles@gmail.com>
2024-06-21 15:03:38 +0300

    Add validators for requests dtos

---
+++ BLL/Validators/OrderRequestDTOValidator.cs
@@ -0,0 +1,14 @@
+﻿using BLL.DTO.Request;
+using FluentValidation;
+
+namespace BLL.Validators;
+
+public class OrderRequestDTOValidator : AbstractValidator<OrderRequestDTO>
+{
+	public OrderRequestDTOValidator()
+	{
+		RuleFor(order => order.UserId)
+			.NotNull()
+			.NotEmpty();
+	}
+}
