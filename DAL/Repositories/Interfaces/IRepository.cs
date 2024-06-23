492cdd837140245e692bfd67cfeca6d74704ba6b
Kigggysha <kirillbel2004@gmail.com>
2024-06-23 21:53:42 +0300

    Created other repositories

--- DAL/Repositories/Interfaces/IRepository.cs
+++ DAL/Repositories/Interfaces/IRepository.cs
@@ -6,7 +6,7 @@
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> FindAsync(Func<T, bool> predicate);
-        Task<int> SaveChangesAsync(T entity);
+        Task SaveChangesAsync(T entity);
         void CreateAsync(T entity);
         void UpdateAsync(T item);
         void DeleteAsync(int id);
