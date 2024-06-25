db4445da7800a33f6db7ea7b69bc41b505f7dff3
Ilya Katushenok <anotherfles@gmail.com>
2024-06-25 14:11:56 +0300

    Add services interfaces

---
+++ BLL/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using BLL.DTO.Request;
+using BLL.DTO.Response;
+
+namespace BLL.Services.Interfaces;
+
+public interface ICategoryService
+{
+	Task<CategoryResponseDTO> CreateAsync(CategoryRequestDTO category);
+	Task UpdateAsync(int id, CategoryRequestDTO category);
+	Task DeleteAsync(int id);
+	Task<CategoryResponseDTO> GetByIdAsync(int id);
+	Task<IEnumerable<CategoryResponseDTO>> GetAllAsync();
+}
---
+++ BLL/Services/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using BLL.DTO.Request;
+using BLL.DTO.Response;
+
+namespace BLL.Services.Interfaces;
+
+public interface IProductService
+{
+	Task<ProductResponseDTO> CreateAsync(ProductRequestDTO product);
+	Task UpdateAsync(int id, ProductRequestDTO product);
+	Task DeleteAsync(int id);
+	Task<ProductResponseDTO> GetByIdAsync(int id);
+}
