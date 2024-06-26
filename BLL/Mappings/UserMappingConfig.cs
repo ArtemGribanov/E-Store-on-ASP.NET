5c8593fe2167ecc0e2fec4ec73e53b161696fda0
Ilya Katushenok <anotherfles@gmail.com>
2024-06-26 23:55:22 +0300

    start make services and mappings

---
+++ BLL/Mappings/UserMappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using BLL.DTO.Response;
+using DAL.Entities;
+using Mapster;
+
+namespace BLL.Mappings;
+
+public class UserMappingConfig : IRegister
+{
+	public void Register(TypeAdapterConfig config)
+	{
+		throw new NotImplementedException();
+		config.NewConfig<User, UserResponseDTO>()
+			.Map(dest => dest.Orders, src => src.Orders);
+	}
+}
---
+++ BLL/Services/Implementations/OrderService.cs
@@ -0,0 +1,49 @@
+﻿using BLL.DTO.Request;
+using BLL.DTO.Response;
+using BLL.Services.Interfaces;
+using DAL.Repositories.Interfaces;
+using FluentValidation;
+
+namespace BLL.Services.Implementations;
+
+public class OrderService : IOrderService
+{
+	private readonly IOrderRepository _orderRepository;
+	private readonly IValidator<OrderRequestDTO> _validator;
+	public async Task<OrderResponseDTO> CreateAsync(OrderRequestDTO order)
+	{
+		var validationResult = await _validator.ValidateAsync(order);
+
+		if (!validationResult.IsValid) 
+		{
+			//exception
+		}
+
+		throw new NotImplementedException();
+	}
+
+	public Task DeleteAsync(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IEnumerable<OrderResponseDTO>> GetAllAsync()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<OrderResponseDTO> GetByIdAsync(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IEnumerable<OrderResponseDTO>> GetByUserIdAsync(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task UpdateAsync(int id, OrderRequestDTO order)
+	{
+		throw new NotImplementedException();
+	}
+}
