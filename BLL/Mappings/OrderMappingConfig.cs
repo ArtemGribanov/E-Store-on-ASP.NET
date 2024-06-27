bfe8af5d5fe25765d6aa43ab1c9c2663aa6ac462
Ilya Katushenok <anotherfles@gmail.com>
2024-06-27 16:56:43 +0300

    Add Order Service

---
+++ BLL/Mappings/OrderMappingConfig.cs
@@ -0,0 +1,13 @@
+﻿using BLL.DTO.Response;
+using DAL.Entities;
+using Mapster;
+
+namespace BLL.Mappings;
+
+public class OrderMappingConfig : IRegister
+{
+	public void Register(TypeAdapterConfig config)
+	{
+		config.NewConfig<OrderResponseDTO, Order>();
+	}
+}
--- BLL/Services/Implementations/OrderService.cs
+++ BLL/Services/Implementations/OrderService.cs
@@ -1,49 +1,130 @@
 ﻿using BLL.DTO.Request;
 using BLL.DTO.Response;
 using BLL.Services.Interfaces;
+using DAL.Entities;
 using DAL.Repositories.Interfaces;
 using FluentValidation;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace BLL.Services.Implementations;
 
 public class OrderService : IOrderService
 {
 	private readonly IOrderRepository _orderRepository;
+	private readonly IUserRepository _userRepository;
 	private readonly IValidator<OrderRequestDTO> _validator;
+	public OrderService(
+		IOrderRepository orderRepository,
+		IUserRepository userRepository,
+		IValidator<OrderRequestDTO> validator)
+	{
+		_orderRepository = orderRepository;
+		_userRepository = userRepository;
+		_validator = validator;
+	}
 	public async Task<OrderResponseDTO> CreateAsync(OrderRequestDTO order)
 	{
 		var validationResult = await _validator.ValidateAsync(order);
 
 		if (!validationResult.IsValid) 
 		{
-			//exception
+			//exception validation error
 		}
 
-		throw new NotImplementedException();
+		var user = _userRepository.GetByIdAsync(order.UserId);
+
+		if (user == null)
+		{
+			//exception user doesnt exist
+		}
+
+		var mappedOrder = order.Adapt<Order>();
+
+		_orderRepository.CreateAsync(mappedOrder);
+
+		//await _orderRepository.SaveChangesAsync(mappedOrder);
+
+		var response = mappedOrder.Adapt<OrderResponseDTO>();
+		return response;
 	}
 
 	public Task DeleteAsync(int id)
 	{
-		throw new NotImplementedException();
+		var order = _orderRepository.GetByIdAsync(id);
+
+		if (order == null)
+		{
+			//exception order doesnt exist
+		}
+
+		_orderRepository.DeleteAsync(id);
+
+		//await _orderRepository.SaveChangesAsync();
+
+		throw new NotImplementedException();//
 	}
 
-	public Task<IEnumerable<OrderResponseDTO>> GetAllAsync()
+	public async Task<IEnumerable<OrderResponseDTO>> GetAllAsync()
 	{
-		throw new NotImplementedException();
+		var orders = await _orderRepository.GetAllAsync();
+		var mappedOrders = orders.Adapt<IEnumerable<OrderResponseDTO>>();
+
+		return mappedOrders;
 	}
 
-	public Task<OrderResponseDTO> GetByIdAsync(int id)
+	public async Task<OrderResponseDTO> GetByIdAsync(int id)
 	{
-		throw new NotImplementedException();
+		var order = await _orderRepository.GetByIdAsync(id);
+
+		if (order == null)
+		{
+			//exception order doesnt exist
+		}
+
+		return order.Adapt<OrderResponseDTO>();
 	}
 
-	public Task<IEnumerable<OrderResponseDTO>> GetByUserIdAsync(int id)
+	public async Task<IEnumerable<OrderResponseDTO>> GetByUserIdAsync(int id)
 	{
-		throw new NotImplementedException();
+		var user = await _userRepository.GetByIdAsync(id);
+
+		if(user == null)
+		{
+			//exception user doesnt exist
+		}
+
+		var orders = await _orderRepository
+			.GetAllAsync()
+			.Result
+			.Where(order => order.Id == id)
+			.AsQueryable()
+			.ToListAsync();
+
+		return orders.Adapt<IEnumerable<OrderResponseDTO>>();
 	}
 
-	public Task UpdateAsync(int id, OrderRequestDTO order)
+	public async Task UpdateAsync(int id, OrderRequestDTO order)
 	{
-		throw new NotImplementedException();
+		var validationResult = _validator.Validate(order);
+
+		if (validationResult.IsValid)
+		{
+			// exception validation error
+		}
+
+		var orderExist = await _orderRepository.GetByIdAsync(id);
+
+		if (orderExist == null)
+		{
+			// exception order doesnt exist
+		}
+
+		order.Adapt(orderExist);
+
+		_orderRepository.UpdateAsync(orderExist);
+
+		//_orderRepository.SaveChangesAsync();
+		//throw new NotImplementedException();
 	}
 }
