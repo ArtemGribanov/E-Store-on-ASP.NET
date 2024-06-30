eb0fa7b6e4751afa651341c02cd5bb7d13b2f281
Ilya Katushenok <anotherfles@gmail.com>
2024-06-30 22:12:15 +0300

    Add Product Service

---
+++ BLL/Services/Implementations/CategoryService.cs
@@ -0,0 +1,42 @@
+﻿using BLL.DTO.Request;
+using BLL.DTO.Response;
+using BLL.Services.Interfaces;
+using DAL.Repositories.Interfaces;
+using FluentValidation;
+
+namespace BLL.Services.Implementations;
+
+public class CategoryService : ICategoryService
+{
+	private readonly ICategoryRepository _categoryRepository;
+	private readonly IValidator<CategoryRequestDTO> _validator;
+    public CategoryService(ICategoryRepository categoryRepository, IValidator<CategoryRequestDTO> validator)
+    {
+        _categoryRepository = categoryRepository;
+		_validator = validator;
+    }
+    public Task<CategoryResponseDTO> CreateAsync(CategoryRequestDTO category)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task DeleteAsync(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IEnumerable<CategoryResponseDTO>> GetAllAsync()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<CategoryResponseDTO> GetByIdAsync(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task UpdateAsync(int id, CategoryRequestDTO category)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ BLL/Services/Implementations/ProductService.cs
@@ -0,0 +1,80 @@
+﻿using BLL.DTO.Request;
+using BLL.DTO.Response;
+using BLL.Exceptions;
+using BLL.Services.Interfaces;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using FluentValidation;
+using Mapster;
+
+namespace BLL.Services.Implementations;
+
+public class ProductService : IProductService
+{
+	private readonly IProductRepository _productRepository;
+	private readonly IValidator<ProductRequestDTO> _validator;
+    public ProductService(IProductRepository productRepository, IValidator<ProductRequestDTO> validator)
+    {
+        _productRepository = productRepository;
+		_validator = validator;
+    }
+    public async Task<ProductResponseDTO> CreateAsync(ProductRequestDTO product)
+	{
+		var validationResult = await _validator.ValidateAsync(product);
+
+		if (validationResult.IsValid)
+		{
+			throw new Exceptions.ValidationException("Validation error");
+		}
+
+		var mappedProduct = product.Adapt<Product>();
+
+		_productRepository.CreateAsync(mappedProduct);
+
+		var response = mappedProduct.Adapt<ProductResponseDTO>();
+		return response;
+	}
+
+	public async Task DeleteAsync(int id)
+	{
+		var product = await _productRepository.GetByIdAsync(id);
+
+		if (product == null)
+		{
+			throw new NotFoundException($"Product with id {id} not found");
+		}
+
+		_productRepository.DeleteAsync(product);
+	}
+
+	public async Task<ProductResponseDTO> GetByIdAsync(int id)
+	{
+		var product = await _productRepository.GetByIdAsync(id);
+
+		if (product == null)
+		{
+			throw new NotFoundException($"Product with id {id} not found");
+		}
+
+		return product.Adapt<ProductResponseDTO>();
+	}
+
+	public async Task UpdateAsync(int id, ProductRequestDTO product)
+	{
+		var validationResult = await _validator.ValidateAsync(product);
+
+		if (validationResult.IsValid)
+		{
+			throw new Exceptions.ValidationException("Validation error");
+		}
+
+		var productExist = await _productRepository.GetByIdAsync(id);
+
+		if (product == null)
+		{
+			throw new NotFoundException($"Product with id {id} not found");
+		}
+
+		_productRepository.UpdateAsync(productExist);
+	}
+}
